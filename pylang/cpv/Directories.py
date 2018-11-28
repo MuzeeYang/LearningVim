3bfa7dfa143d20345b73b5e19ea10bb28256df53
Leo <liyang_oszz@163.com>
2018-11-28 14:01:09 +0800

    cpv update

--- pylang/cpv/Directories.py
+++ pylang/cpv/Directories.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import os
 
+argv = os.sys.argv
+
 def DetectDir(path):
-	for rootPath, dirs, files in os.walk(path, followlinks=True):
+	for rootPath, dirs, files in os.walk(path, followlinks=False):
 		for f in files:
-			yield ('f', ExtractDir(os.path.join(rootPath, f), path))
+			fullPath = os.path.join(rootPath, f)
+			yield (GetFileType(fullPath), ExtractDir(fullPath, path))
 		for d in dirs:
 			yield ('d', ExtractDir(os.path.join(rootPath, d), path))
 	return
@@ -21,9 +24,16 @@ def CopyFile(dFile, sFile, fSize):
 	except:
 		return -1
 
+def CopySLink(dFile, sFile):
+	try:
+		os.symlink(os.readlink(sFile), dFile)
+		return 0
+	except:
+		return -1
+
 def GetFileInfo(f):
 	try:
-		os_res = os.stat(f)
+		os_res = os.lstat(f)
 		return (int(os_res.st_mtime), os_res.st_size)
 	except:
 		return ()
@@ -34,3 +44,40 @@ def ExtractDir(sPath, xPath):
 	sPathLst = [e for e in sPath.split(sep=os.sep)[1:] if e]
 	return os.sep.join(sPathLst[len(xPathLst):])
 
+def JoinDir(sPath, xPath):
+	return os.path.join(sPath, xPath)
+
+def GetFileType(f):
+	if(os.path.isdir(f)):
+		return 'd'
+	elif(os.path.islink(f)):
+		return 'l'
+	elif(os.path.isfile(f)):
+		return 'f'
+	else:
+		return 'x'
+
+def RemoveDir(path):
+	for rootPath, dirs, files in os.walk(path, followlinks=False):
+		for f in files:
+			os.remove(f)
+	os.removedirs(path)
+
+def RemoveAll(path):
+	fType = GetFileType(path)
+	if(fType == 'x'):
+		pass
+	elif(fType == 'd'):
+		RemoveDir(path)
+	else:
+		os.remove(path)
+
+def MakeDir(path):
+	fType = GetFileType(path)
+	if(fType == 'd'):
+		return
+	elif(fType == 'x'):
+		pass
+	else:
+		os.remove(path)
+	os.mkdir(path)
--- pylang/cpv/cpv.py
+++ pylang/cpv/cpv.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python3
 import Directories as ds
-import os
 import time
 
-def CopyFileWrapper(dFile, sFile, fSize, newFile):
+def CopyFileWrapper(dFile, sFile, fSize, fType, newFile):
 	print('[{time}]{file}: '.format(time=time.strftime('%H:%M:%S'), file=newFile), end='')
-	if(ds.CopyFile(dFile, sFile, fSize) == 0):
+	if(fType == 'f' and ds.CopyFile(dFile, sFile, fSize) == 0):
+		print('OK')
+	elif(fType == 'l' and ds.CopySLink(dFile, sFile) == 0):
 		print('OK')
 	else:
 		print('Failed')
 	
 
 def main():
-	if(len(os.sys.argv) < 3):
+	if(len(ds.argv) < 3):
 		print('usage wrong!')
 		return
-	elif(not os.path.isdir(os.sys.argv[1])):
-		print('%s: not exists!' % os.sys.argv[1])
+	elif(not ds.GetFileType(ds.argv[1]) == 'd'):
+		print('%s: not exists!' % ds.argv[1])
 		return
-	elif(not os.path.isdir(os.sys.argv[2])):
-		print('%s: not exists!' % os.sys.argv[2])
+	elif(not ds.GetFileType(ds.argv[2]) == 'd'):
+		print('%s: not exists!' % ds.argv[2])
 		return
 
 	print('====' + time.strftime('%Y/%m/%d') + '====')
-	for tp, ffile in ds.DetectDir(os.sys.argv[1]):
-		orcPath = os.path.join(os.sys.argv[1], ffile)
-		modPath = os.path.join(os.sys.argv[2], ffile)
+	for tp, ffile in ds.DetectDir(ds.argv[1]):
+		orgPath = ds.JoinDir(ds.argv[1], ffile)
+		modPath = ds.JoinDir(ds.argv[2], ffile)
 		if(tp == 'd'):
-			if(os.path.isdir(modPath)):
-				pass
-			else:
-				os.mkdir(modPath)
+			ds.MakeDir(modPath)
 		else:
-			orcFileCtime, orcFileSize = ds.GetFileInfo(orcPath)
-			if(os.path.exists(modPath)):
-				modFileCtime, modFileSize = ds.GetFileInfo(modPath)
-				if(orcFileCtime > modFileCtime):
-					CopyFileWrapper(modPath, orcPath, orcFileSize, ffile)
+			modType = ds.GetFileType(modPath)
+			orgFileCtime, orgFileSize = ds.GetFileInfo(orgPath)
+			if(modType == 'x'):
+				CopyFileWrapper(modPath, orgPath, orgFileSize, tp, ffile)
+			elif(modType == 'd'):
+				ds.RemoveDir(modPath)
+				CopyFileWrapper(modPath, orgPath, orgFileSize, tp, ffile)
 			else:
-				CopyFileWrapper(modPath, orcPath, orcFileSize, ffile)
+				modFileCtime, modFileSize = ds.GetFileInfo(modPath)
+				if(orgFileCtime > modFileCtime):
+					CopyFileWrapper(modPath, orgPath, orgFileSize, tp, ffile)
 	
 
 if(__name__ == '__main__'):
