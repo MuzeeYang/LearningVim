34d4510b94848054fd7114c7a6c71a472e9a7b4c
Leo <liyang_oszz@163.com>
2019-05-10 17:47:28 +0800

    skiString modify

--- clang/skiString.c
+++ clang/skiString.c
@@ -1,14 +1,113 @@
 #include "stdlib.h"
 #include "string.h"
+#include "skiString.h"
+#include "regex.h"
 
-PStrWrapper wrapString(char* buf, int size, char* sep)
+char* initBuffer(char* str, unsigned int size)
+{
+	PBuffer buffer = NULL;
+
+	if(str){
+		buffer = (PBuffer)(str - sizeof(TBuffer));
+		if(buffer->size < size){
+			size = BUFFER_SIZE(size);
+			if((buffer = realloc(buffer, size + sizeof(TBuffer))) == NULL){
+				goto BUFFER_REALLOC_FAILED;
+			}
+
+			buffer->size = size;
+			str = buffer->buf;
+		}
+	}else{
+		size = BUFFER_SIZE(size);
+		if((buffer = malloc(size + sizeof(TBuffer))) == NULL){
+			goto BUFFER_MALLOC_FAILED;
+		}
+
+		buffer->size = size;
+		str = buffer->buf;
+	}
+
+	return str;
+
+BUFFER_REALLOC_FAILED:
+BUFFER_MALLOC_FAILED:
+	return NULL;
+}
+
+unsigned int measureBuffer(char* str)
+{
+	PBuffer buffer = (PBuffer)(str - sizeof(TBuffer));
+
+	return buffer->size;
+}
+
+int freeBuffer(char* str)
+{
+	PBuffer buffer = (PBuffer)(str - sizeof(TBuffer));
+
+	if(buffer->size < STR_BLK_SIZE || buffer->size%STR_BLK_SIZE != 0){
+		return -1;
+	}
+
+	free(buffer);
+	return 0;
+}
+
+PStrWrapper regString(char* buf, char* regstr)
+{
+	regex_t reg = {0};
+	PStrWrapper strw = NULL;
+	//regmatch_t pmatch[12] = {0};
+
+	if(regcomp(&reg, regstr, REG_NEWLINE)){
+		return NULL;
+		//goto REG_COMPILE_FAILED;
+	}
+
+	regmatch_t pmatch[reg.re_nsub + 1];
+	if(regexec(&reg, buf, reg.re_nsub + 1, pmatch, 0)){
+		goto REG_EXECUTE_FAILED;
+	}
+
+	if((strw = malloc(sizeof(TStrWrapper) + sizeof(char*) * (reg.re_nsub+1)*2)) == NULL){
+		goto WRAPPER_MALLOC_FAILED;
+	}
+
+	memset(strw, 0, sizeof(TStrWrapper));
+
+	strw->buf = buf;
+	strw->sep = regstr;
+	strw->num = (reg.re_nsub+1) * 2;
+
+	int i, j;
+	for(i = 0, j = 0; i < reg.re_nsub + 1; i++){
+		strw->field[j++] = buf + pmatch[i].rm_so;
+		strw->field[j++] = buf + pmatch[i].rm_eo;
+	}
+
+	regfree(&reg);
+	return strw;
+
+	free(strw);
+	strw = NULL;
+WRAPPER_MALLOC_FAILED:
+REG_EXECUTE_FAILED:
+	regfree(&reg);
+REG_COMPILE_FAILED:
+	return NULL;
+}
+
+PStrWrapper wrapString(char* buf, char* sep)
 {
 	PStrWrapper strw = NULL;
 	char* sbuf = NULL;
 	char* scur = NULL;
 	int sepLen = 0;
 	int snum = 0;
+	int size = 0;
 
+	size = strlen(buf) + 1; 
 	if((sbuf = malloc(size)) == NULL){
 		goto BUFFER_MALLOC_FAILED;
 	}
@@ -53,8 +152,8 @@ BUFFER_MALLOC_FAILED:
 int freeString(PStrWrapper strw)
 {
 	if(strw){
-		free(strw->buf);
-		//free(strw->sep);
+		if(strw->buf && strw->size)
+			free(strw->buf);
 		free(strw);
 	}
 	return 0;
@@ -100,5 +199,29 @@ BUFFER_REALLOC_FAILED:
 	free(str);
 BUFFER_MALLOC_FAILED:
 	return NULL;
+}
+
+char* lowerString(char* str)
+{
+	char* cur = str;
+	while(*cur){
+		if(*cur > 0x40 && *cur < 0x5b){
+			*cur += 0x20;
+		}
+		cur++;
+	}
+	return str;
+}
+
+char* upperString(char* str)
+{
+	char* cur = str;
+	while(*cur){
+		if(*cur > 0x60 && *cur < 0x7b){
+			*cur -= 0x20;
+		}
+		cur++;
+	}
+	return str;
 }
 
--- clang/skiString.h
+++ clang/skiString.h
@@ -2,6 +2,13 @@
 #define __SKISTRING_H__
 
 #define STR_BLK_SIZE (0x400)
+#define BUFFER_SIZE(size) (((size)&~(STR_BLK_SIZE-1)) + (((size)&(STR_BLK_SIZE-1)) != 0)*STR_BLK_SIZE)
+//#define BUFFER_SIZE(size) ((size)/STR_BLK_SIZE*STR_BLK_SIZE + ((size)%STR_BLK_SIZE != 0)*STR_BLK_SIZE)
+
+typedef struct _strBuffer{
+	unsigned int size;
+	char buf[0];
+}TBuffer, *PBuffer;
 
 typedef struct _strWrapper{
 	char* buf;
@@ -11,8 +18,15 @@ typedef struct _strWrapper{
 	char* field[0];
 }TStrWrapper, *PStrWrapper;
 
-PStrWrapper wrapString(char* buf, int size, char* sep);
+char* initBuffer(char* str, unsigned int size);
+unsigned int measureBuffer(char* str);
+int freeBuffer(char* str);
+
+PStrWrapper regString(char* buf, char* regstr);
+PStrWrapper wrapString(char* buf, char* sep);
 int freeString(PStrWrapper strw);
 char* joinString(char** field, int size, char* sep);
+char* lowerString(char* str);
+char* upperString(char* str);
 
 #endif //skiString.h
